175d0ec2b57d19ca634d7045b7713b00775cb979
chusi <4us.in.ua@gmail.com>
2022-04-23 12:13:38 +0300

    Add project files.

---
+++ Homework2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Выполнил Цубера Кристиан
+/// </summary>
+
+namespace Homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { }
+    }
+}
